dd1620f34de483657ce5979664da15334bd4c701
Thanuseehan <sttthanuseehan@gmail.com>
2025-03-05 01:50:40 +0530

    huj

---
+++ backend/models/Player.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const playerSchema = new mongoose.Schema({
+  key: { type: Number, required: true, unique: true },
+  playerName: { type: String, required: true },
+  playerType: { type: String, default: "N/A" },
+  age: { type: Number, required: true },
+  basePrice: { type: Number, required: true },
+  battingStyle: { type: String, default: "N/A" },
+  bowlingStyle: { type: String, default: "N/A" },
+  originTeam: { type: String },
+  previousTournament: { type: String },
+  profilePicture: { type: String },
+});
+
+const Player = mongoose.model("Player", playerSchema);
+module.exports = Player;
--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -1,32 +1,34 @@
 import React from 'react'
 import { assets } from "../assets/assets"; // Ensure correct path
+import LiveHeader from "../components/LiveHeader"
 
 
 const Preview = () => {
-  const backgroundStyle = {
-          backgroundImage: `url(${assets.Bgimg})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          width: "100vw",
-          height: "100vh",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        };
-      
-        return (
-          <div style={backgroundStyle}>
-          <LiveHeader leftTitle="VM Trophy Session 3 - 2025" rightTitle="KFM Broadcast" />
-    
-          <div style={{ marginTop: "100px", padding: "20px", color: "white", textAlign: "center" }}>
-            <h1>Welcome to the Preview Dashboard</h1>
-          </div>
-    
-          {/* Centered Image at Bottom */}
-        </div>
-        );
-    }
+  
+    const backgroundStyle = {
+      backgroundImage: `url(${assets.Bgimg})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      backgroundRepeat: "no-repeat",
+      width: "100vw",
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    };
+  
+    return (
+      <div style={backgroundStyle}>
+      <LiveHeader leftTitle="VM Trophy Session 3 - 2025" rightTitle="KFM Broadcast" />
 
+      <div style={{ marginTop: "100px", padding: "20px", color: "white", textAlign: "center" }}>
+        <h1>Welcome to the Preview Dashboard</h1>
+      </div>
+
+      {/* Centered Image at Bottom */}
+    </div>
+
+  )
+}
 
 export default Preview
